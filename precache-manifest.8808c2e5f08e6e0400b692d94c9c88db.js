53a3f33e1b31fb7f68483afd54c61a13323e3eb2
Tyler Mac <tdmam404@outlook.com>
2020-05-07 10:18:32 -0500

    Updates

---
+++ precache-manifest.8808c2e5f08e6e0400b692d94c9c88db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02faf79cbd889d0d1a3abac61c600076",
+    "url": "/drip-shop/index.html"
+  },
+  {
+    "revision": "f9ed6e72b0c5e6644fdd",
+    "url": "/drip-shop/static/css/main.3d81fd4e.chunk.css"
+  },
+  {
+    "revision": "31fb837eb380b7ad9a94",
+    "url": "/drip-shop/static/js/2.d6b955a1.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/drip-shop/static/js/2.d6b955a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ed6e72b0c5e6644fdd",
+    "url": "/drip-shop/static/js/main.ca50c81f.chunk.js"
+  },
+  {
+    "revision": "af8ee79fe0d7c562cc0e",
+    "url": "/drip-shop/static/js/runtime-main.f6dae082.js"
+  },
+  {
+    "revision": "4bee4f3abd1d95aebac5e7dcafc77540",
+    "url": "/drip-shop/static/media/blank_shirt.4bee4f3a.png"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/drip-shop/static/media/logo.8dad8028.svg"
+  }
+]);
